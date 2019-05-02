23bb7229d9593c8c3c44183df487ab1364191135
László Matuska <info@bitofgold.com>
2019-05-02 19:43:18 +0200

    correct center and right align, padding, alpha

--- PixelFontCanvas.js
+++ PixelFontCanvas.js
@@ -1,6 +1,6 @@
 class PixelFontCanvas {
 
-    //Loads a .fnt font file and an image from font_dir (wih slash!) directory, registers font by font face
+    //Loads a .fnt font file and an image from font_dir (with slash!) directory, registers font by font face
     static loadFont(font_dir, font_file, on_success = function(data) {}) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
@@ -26,16 +26,40 @@ class PixelFontCanvas {
         const data = PixelFontCanvas._registerFontXML(xml,image_obj);
     }
 
+    static fontSize(font) {
+        const data = PixelFontCanvas.fonts[font]
+        return data.size
+    }
+
+    static _removeItems(arr, startIdx, removeCount) {
+        const length = arr.length;
+        let i;
+    
+        if (startIdx >= length || removeCount === 0) { return; }
+    
+        removeCount = (startIdx + removeCount > length ? length - startIdx : removeCount);
+    
+        const len = length - removeCount;
+    
+        for (i = startIdx; i < len; ++i) {
+            arr[i] = arr[i + removeCount];
+        }
+    
+        arr.length = len;
+    }
+
     //draws text to canvas, style and position in style
     static drawText(canvas, text, style={}) {
         style = {
             font: style.font,
-            x: style.x !== undefined ? style.x : 0,
-            y: style.y !== undefined ? style.y : 0,
-            width: style.width !== undefined ? style.width : canvas.width,
-            scale: style.scale !== undefined ? style.scale : 1,
+            x: style.x !== undefined ? Math.round(style.x) : 0,
+            y: style.y !== undefined ? Math.round(style.y) : 0,
+            width: style.width !== undefined ? Math.round(style.width) : Math.round(canvas.width),
+            scale: style.scale !== undefined ? Math.round(style.scale) : 1,
             align: style.align || 'left',
             tint: style.tint !== undefined ? style.tint : '#FFFFFF',
+            alpha: style.alpha !== undefined ? style.alpha : 1.0,
+            padding: style.padding !== undefined ? style.padding : 0,
         };
         const data = PixelFontCanvas.fonts[style.font];
         const scale = style.scale;
@@ -51,7 +75,6 @@ class PixelFontCanvas {
         let lastSpace = -1;
         let lastSpaceWidth = 0;
         let spacesRemoved = 0;
-        let maxLineHeight = 0;
 
         for (let i = 0; i < text.length; i++)
         {
@@ -66,7 +89,6 @@ class PixelFontCanvas {
             if (/(?:\r\n|\r|\n)/.test(text.charAt(i)))
             {
                 lineWidths.push(lastLineWidth);
-                maxLineWidth = Math.max(maxLineWidth, lastLineWidth);
                 line++;
 
                 pos.x = 0;
@@ -75,16 +97,14 @@ class PixelFontCanvas {
                 continue;
             }
 
-            if (lastSpace !== -1 && style.width > 0 && pos.x * scale > style.width)
+            if (lastSpace !== -1 && style.width > 0 && pos.x * scale > style.width - 2 * style.padding)
             {
-                //todo
-                //core.utils.removeItems(chars, lastSpace - spacesRemoved, i - lastSpace);
+                this._removeItems(chars, lastSpace - spacesRemoved, i - lastSpace);
                 i = lastSpace;
                 lastSpace = -1;
                 ++spacesRemoved;
 
                 lineWidths.push(lastSpaceWidth);
-                maxLineWidth = Math.max(maxLineWidth, lastSpaceWidth);
                 line++;
 
                 pos.x = 0;
@@ -111,14 +131,16 @@ class PixelFontCanvas {
                 position: {x:pos.x+charData.xOffset, y:pos.y+charData.yOffset},
                 charData: charData
             });
-            lastLineWidth = pos.x + (data.texture.naturalWidth + charData.xOffset);
             pos.x += charData.xAdvance;
-            maxLineHeight = Math.max(maxLineHeight, (charData.yOffset + data.texture.naturalHeight));
+            lastLineWidth = pos.x;
             prevCharCode = charCode;
         }
 
         lineWidths.push(lastLineWidth);
-        maxLineWidth = Math.max(maxLineWidth, lastLineWidth);
+
+        for (let i = 0; i <= line; i++) {
+            maxLineWidth = Math.max(maxLineWidth, lineWidths[i]);
+        }
 
         const lineAlignOffsets = [];
 
@@ -126,13 +148,15 @@ class PixelFontCanvas {
         {
             let alignOffset = 0;
 
-            if (style.align === 'right')
+            if (style.align == 'right')
             {
-                alignOffset = maxLineWidth - lineWidths[i];
+                alignOffset = style.width - style.padding - lineWidths[i] * scale;
             }
-            else if (style.align === 'center')
+            else if (style.align == 'center')
             {
-                alignOffset = (maxLineWidth - lineWidths[i]) / 2;
+                alignOffset = Math.round((style.width - lineWidths[i] * scale) / 2);
+            } else {
+                alignOffset = style.padding
             }
 
             lineAlignOffsets.push(alignOffset);
@@ -145,7 +169,7 @@ class PixelFontCanvas {
         {
             let c = {position: {x: 0,y: 0}, rect: chars[i].charData.textureRect, scale: {x:0, y: 0}, tint: '#FFFFFF'};
             _glyphs.push(c);
-            c.position.x = (chars[i].position.x + lineAlignOffsets[chars[i].line]) * scale;
+            c.position.x = chars[i].position.x * scale +  lineAlignOffsets[chars[i].line];
             c.position.y = chars[i].position.y * scale;
             c.scale.x = c.scale.y = scale;
         }
@@ -156,14 +180,13 @@ class PixelFontCanvas {
         drawData.texture = data.texture;
         drawData.font = style.font;
         drawData.tint = style.tint;
+        drawData.alpha = style.alpha;
 
         for (let i = 0; i < lenChars; i++)
         {
             _glyphs[i].position.x += style.x;
             _glyphs[i].position.y += style.y;
         }
-
-        drawData.maxLineHeight = maxLineHeight * scale;
 
         PixelFontCanvas._renderText(canvas, drawData, _glyphs);
     }
@@ -193,17 +216,20 @@ class PixelFontCanvas {
             btx.fillRect(0, 0, el.width, el.height);
             
             // Finally, fix masking issues you'll probably incur and optional globalAlpha
-            btx.globalAlpha = 1;
+            btx.globalAlpha = 1.0;
             btx.globalCompositeOperation = 'destination-in';
             btx.drawImage(texture, 0, 0);
             PixelFontCanvas._texture_cache[drawData.font+'-'+drawData.tint] = el;
         }
-        
+
+        ctx.save();
+        ctx.globalAlpha = drawData.alpha;
         const lenGlyphs = glyphs.length;
         for (let i = 0; i < lenGlyphs; i++) {
             let g = glyphs[i];
             ctx.drawImage(el,g.rect.x,g.rect.y,g.rect.width,g.rect.height,g.position.x,g.position.y,g.rect.width*g.scale.x,g.rect.height*g.scale.y);
         }
+        ctx.restore();
     }
 
     static _fntToXML(text) {
